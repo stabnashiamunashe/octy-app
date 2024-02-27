9134419fc5cec9ec8c43696f5b829544b014ff28
Munashe Stabnashia Nzira <stabnashiamunashe@gmail.com>
2024-02-27 18:22:28 +0200

    Update

--- extensions/web-pixel/src/config/endpoints.js
+++ extensions/web-pixel/src/config/endpoints.js
@@ -1,13 +1,13 @@
-const octyAppHost = process.env.OCTY_APP_HOST
+const octyAppHost = "https://pod1.sonity.net/octy-shopify";
 
 module.exports = {
   octyAppHost,
-  createCustomerURI: octyAppHost + '/api/customers/createupdate',
-  createEventURI: octyAppHost + '/api/hooks/events/create',
-  createItemURI: octyAppHost + '/api/hooks/items/create',
-  updateItemURI: octyAppHost + '/api/hooks/items/update',
-  deleteItemURI: octyAppHost + '/api/hooks/items/delete',
-  getContentURI: octyAppHost + '/api/content',
-  getRecommendationsURI: octyAppHost + '/api/recommendations',
-  pixelURI: octyAppHost + '/api/hooks/events/create',
-}
+  createCustomerURI: octyAppHost + "/api/customers/createupdate",
+  createEventURI: octyAppHost + "/api/hooks/events/create",
+  createItemURI: octyAppHost + "/api/hooks/items/create",
+  updateItemURI: octyAppHost + "/api/hooks/items/update",
+  deleteItemURI: octyAppHost + "/api/hooks/items/delete",
+  getContentURI: octyAppHost + "/api/content",
+  getRecommendationsURI: octyAppHost + "/api/recommendations",
+  pixelURI: octyAppHost + "/api/hooks/events/create",
+};
